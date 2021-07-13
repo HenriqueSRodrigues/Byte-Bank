7efd4c96ae3ef8316469d7e4b70f03db71a0f119
HenriqueSRodrigues <henriquesilvarodrigues.hsr@gmail.com>
2021-07-13 15:37:03 -0300

    primeiro commmit

---
+++ bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+@Component({
+  selector: 'app-nova-tranferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
+})
+export class NovaTransferenciaComponent {
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number | undefined;
+  destino: number | undefined;
+
+  transferir() {
+    console.log('solicitada nova transferencia');
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTransferir.emit(valorEmitir);
+    this.limparCampos();
+  }
+
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
+  }
+}
